99c8ba050ddbeddde28ea5e6f4fe1df05d62e4b8
RikibabManos <b.osman.g@outlook.com>
2026-09-23 19:29:46 +0100

    Correct plasma_display.py display time

--- scripts/plasma_display.py
+++ scripts/plasma_display.py
@@ -244,7 +244,8 @@ def update(frame):
         p_max_history.clear()
     
     current_global_state = np.fromfile(data_file_list[frame], dtype = np.float64)
-    current_time = frame * int(frame_rate) * time_between_frames
+    current_timestep = current_global_state[6]
+    current_time = frame * int(frame_rate) * current_time_step
     time_history.append(current_time)
     main_title.set_text(f"2D MHD Simulation Dashboard (Time: {current_time:.3f}s)")
 
